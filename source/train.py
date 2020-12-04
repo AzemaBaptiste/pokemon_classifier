3c03cb9e29493e46df02f489b2ba550316afcf66
Baptiste Azema <baptiste@azema.tech>
2020-12-04 02:05:04 +0100

    notebook

--- source/train.py
+++ source/train.py
@@ -1,56 +1,85 @@
 import argparse
-import json
 import os
-import pandas as pd
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision import datasets, transforms
 
 from model import Net
 
-def _get_train_data_loader():
+
+def _get_train_data_loader(data_dir, valid_size=0.2):
     print("Get train data loader.")
-    return torch.load('train_dataloader.pth')
+    transform_train = transforms.Compose([transforms.Resize(255),
+                                          transforms.CenterCrop(250),
+                                          #    transforms.RandomHorizontalFlip(),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+
+    transform_test = transforms.Compose([transforms.Resize(255),
+                                         transforms.CenterCrop(250),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+
+    train_data = datasets.ImageFolder(data_dir, transform=transform_train)
+    test_data = datasets.ImageFolder(data_dir, transform=transform_test)
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+    np.random.shuffle(indices)
+    train_idx, test_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    trainloader = torch.utils.data.DataLoader(train_data,
+                                              sampler=train_sampler, batch_size=4)
+    testloader = torch.utils.data.DataLoader(test_data,
+                                             sampler=test_sampler, batch_size=4)
+
+    return trainloader, testloader
+
 
 def train(model, train_loader, epochs, criterion, optimizer, device):
+    model.train()
     for epoch in range(epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
 
-    running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
 
-        inputs.to(device)
-        labels.to(device)
-        
-        # zero the parameter gradients
-        optimizer.zero_grad()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
 
     print('Finished Training')
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # SageMaker parameters, like the directories for training data and saving models; set automatically
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    
+
     # Training Parameters, given
     parser.add_argument('--batch-size', type=int, default=4, metavar='N',
                         help='input batch size for training (default: 4)')
@@ -68,24 +97,23 @@ if __name__ == '__main__':
     torch.manual_seed(args.seed)
 
     # Load the training data.
-    train_loader = _get_train_data_loader()
-    
+    train_loader, test_loader = _get_train_data_loader(args.data_dir)
+
     model = Net().to(device)
-    
+
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     # Trains the model (given line of code, which calls the above training function)
     train(model, train_loader, args.epochs, criterion, optimizer, device)
 
-    ## TODO: complete in the model_info by adding three argument names, the first is given
-    # Keep the keys of this dictionary as they are 
+    # Keep the keys of this dictionary as they are
     model_info_path = os.path.join(args.model_dir, 'model_info.pth')
     with open(model_info_path, 'wb') as f:
         model_info = {
         }
         torch.save(model_info, f)
-        
+
     # Save the model parameters
     model_path = os.path.join(args.model_dir, 'model.pth')
     with open(model_path, 'wb') as f:
