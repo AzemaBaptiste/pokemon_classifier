c1f4aa73b9e467bffa89df9e097f79f72aef71b3
Baptiste Azema <baptiste@azema.tech>
2020-12-13 15:37:39 +0100

    notebooks

--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
+import copy
+import time
+
 import cv2
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import torch
 import torchvision
 from sklearn.datasets import load_files
-import numpy as np
+from torchvision import transforms
+from tqdm import tqdm
+import matplotlib.ticker as mtick
 
 
 def load_dataset(path):
@@ -13,6 +20,16 @@ def load_dataset(path):
                   how='left', left_on='target', right_index=True)
     df = pd.concat([df, pd.DataFrame(data.filenames, columns=['filename'])], axis=1, sort=False)
     return df
+
+
+def load_dataset_cifar(cifar100):
+    return pd.merge(
+        pd.DataFrame(cifar100.targets, columns=['target']),
+        pd.DataFrame(cifar100.classes, columns=['label']),
+        how='left',
+        left_on='target',
+        right_index=True,
+    )
 
 
 def imshow(img):
@@ -74,7 +91,7 @@ def detect_human_faces(file_list):
 
 
 def face_detector(img_path):
-    '''returns "True" if face is detected in image stored at img_path'''
+    """returns "True" if face is detected in image stored at img_path"""
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
@@ -99,3 +116,189 @@ def imshow_data_loader(data_loader, std_nums, mean_nums, classes_list):
     # Make a grid from batch
     out = torchvision.utils.make_grid(inputs)
     imshow_tensor(out, std_nums, mean_nums, title=[classes_list[x] for x in classes])
+
+
+def torch_transformations():
+    mean_nums = [0.485, 0.456, 0.406]
+    std_nums = [0.229, 0.224, 0.225]
+
+    transform = {'train': transforms.Compose([
+        transforms.RandomResizedCrop(size=64),
+        transforms.RandomRotation(degrees=15),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean_nums, std_nums)
+
+    ]), 'val': transforms.Compose([
+        transforms.Resize(size=64),
+        transforms.CenterCrop(size=64),
+        transforms.ToTensor(),
+        transforms.Normalize(mean_nums, std_nums)
+
+    ]), 'test': transforms.Compose([
+        transforms.Resize(size=64),
+        transforms.CenterCrop(size=64),
+        transforms.ToTensor(),
+        transforms.Normalize(mean_nums, std_nums)
+    ]),
+    }
+    return transform, std_nums, mean_nums
+
+
+def train_model(model, data_loaders, dataset_sizes, criterion, optimizer, scheduler, device, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in data_loaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+def visualize_model(model, data_loaders, classes, std_nums, mean_nums, device, num_images=6):
+    was_training = model.training
+    model.eval()
+    plt.figure()
+
+    with torch.no_grad():
+        images_so_far = 0
+        for i, (inputs, labels) in enumerate(data_loaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images // 2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}, ground truth: {}'
+                             .format(classes[preds[j]],
+                                     classes[labels[j]]))
+                imshow_tensor(inputs.cpu().data[j], std_nums, mean_nums)
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
+
+def global_accuracy(model, data_loader, classes):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in tqdm(data_loader):
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
+    
+def accuracy_per_class(model, data_loader, classes):
+    class_correct = [0. for i in range(len(classes))]
+    class_total = [0. for i in range(len(classes))]
+    with torch.no_grad():
+        for data in tqdm(data_loader):
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(3):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(len(classes)):
+        if class_total[i] == 0.0:
+            yield classes[i], 0
+        else:
+            yield classes[i], 100 * class_correct[i] / class_total[i]
+
+def test_accuracy(model, data_loader, classes, figsize=(25, 8)):
+    global_accuracy(model, data_loader, classes)
+    
+    res = accuracy_per_class(model, data_loader, classes)
+    df = pd.DataFrame(res)
+
+    x = np.arange(len(df))
+
+    width = 0.4
+
+    fig = plt.figure(figsize=figsize)
+    ax = plt.gca()
+    ax.set_title('Accuracy per class')
+    rects = ax.bar(x - width / 2, df[1], width)
+    plt.xticks(x, df[0], rotation=90)
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=100))
+    def autolabel(rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = int(rect.get_height())
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height +1),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+    autolabel(rects)
+    plt.show()
